14f0380293aa92a55d9e55abe6ee4c9dd9ee4de7
Tim <tim-gerling@gmx.de>
2025-04-20 23:18:48 +0200

    Clean up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
 #include "sensors.hpp"
+#include "utils.hpp"
 
-void printCSVWithTimestamp(float* values, size_t size){
-  Serial.print(millis()); // Print timestamp
-  Serial.print(",");
-  for (size_t i = 0; i < size; ++i) {
-    Serial.print(values[i], 3);
-    if (i < size - 1) Serial.print(",");
-  }
-  Serial.println();
-}
+#define LED_PIN 8
+#define TEMP_THRESHOLD_HIGH 28.0 // Temperature threshold for LED indication
+#define TEMP_THRESHOLD_LOW 27.9  
+
+bool ledState = false; // LED state
 
 void setup() {
   Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+
   initSensors(); // Initialize sensors
   calibrateSensors(); // Calibrate sensors
 }
@@ -20,6 +19,16 @@ void setup() {
 void loop() {
   GyroData gyroData = readGyro();         // in °/s
   MagData magData = readMagnetometer();  // raw 
+
+  float smoothedTemp = getSmoothedTemperature(gyroData.temperature);
+
+  if (!ledState && smoothedTemp > TEMP_THRESHOLD_HIGH) {
+    digitalWrite(LED_PIN, HIGH);
+    ledState = true;
+  } else if (ledState && smoothedTemp < TEMP_THRESHOLD_LOW) {
+    digitalWrite(LED_PIN, LOW);
+    ledState = false;
+  };
    
   float transmissionData[] = {
     gyroData.gyroX, gyroData.gyroY, gyroData.gyroZ,
